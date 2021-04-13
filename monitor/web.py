0b7d20a06503ec1c5eda5f0c5426a11ec2b8345c
Christophe VG <contact@christophe.vg>
2021-04-13 14:37:33 +0200

    open up CORS

--- monitor/web.py
+++ monitor/web.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 def default(name=None):
   return render_template("index.html")
 
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 def publish(packet):
   try:
